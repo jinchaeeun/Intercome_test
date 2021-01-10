4019f80ccb2d96560e54f4bdd83ff2422973ce00
jinchaeeun <kbg05032@naver.com>
2021-01-10 21:07:30 +0900

    버튼 진동 이벤트

--- app/src/main/java/com/example/intercome_test/MainActivity.kt
+++ app/src/main/java/com/example/intercome_test/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intercome_test
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        but_vib.setOnClickListener {
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, 50))
+        }
+
     }
 }
